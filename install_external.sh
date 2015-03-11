f94f88f6a9fd8204e93ef26e9401a84d3d3b74a7
enthought <enthought@simphony-base.(none)>
2015-03-11 20:56:38 +0000

    fix symlink creation

--- install_external.sh
+++ install_external.sh
@@ -13,14 +13,14 @@ git fetch --tags
 git checkout r12824
 cd src
 make ubuntu_simple
-ln -s lmp_ubuntu_simple ~/simphony/bin/lammps
+ln -s $(pwd)/lmp_ubuntu_simple ~/simphony/bin/lammps
 popd
 
 # Install JYU-LB
 git clone https://github.com/simphony/JYU-LB.git
 pushd JYU-LB
 make
-ln -s ./bin/jyu_lb_isotherma3D.exe ~/simphony/bin/jyu_lb_isotherma3D.exe
+ln -s $(pwd)/bin/jyu_lb_isothermal3D.exe ~/simphony/bin/jyu_lb_isothermal3D.exe
 popd
 
 # install PyFoam
