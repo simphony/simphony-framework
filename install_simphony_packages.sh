1c859a6c891c622c98676479bf321a66349e65cf
enthought <enthought@simphony-base.(none)>
2015-03-20 16:42:16 +0000

    update build scripts

--- install_simphony_packages.sh
+++ install_simphony_packages.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-source ~/simphony/bin/activate
-
 pip --version
-# (1) Install cython and numexpr externally because the requirements does
-#     work with them
-# (2) Pytables breaks with latest Cython
-#     see https://github.com/PyTables/PyTables/issues/388
-pip install numexpr cython==0.20
+pip install numexpr
 pip install -r requirements.txt
 pip install -r simphony_packages.txt
