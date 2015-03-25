c44c2da4dd4a352353bebc99a27d5df3293ad999
enthought <enthought@simphony-base.(none)>
2015-03-25 16:43:53 +0000

    fix numexpr version

--- install_simphony_packages.sh
+++ install_simphony_packages.sh
@@ -4,6 +4,6 @@ set -e
 pip --version
 # (1) Install numexpr externally because the requirements do not
 #     work with them
-pip install numexpr
+pip install "numexpr>=2.0.0"
 pip install -r requirements.txt
 pip install -r simphony_packages.txt
