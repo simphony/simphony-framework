cfc25bc1e32c7001bc488532d76f804a6ef9f4cb
Ioannis Tziakos <ioannist@enthought.com>
2015-04-16 13:41:35 +0100

    update requirements to install PIL

--- install_apt_requirements.sh
+++ install_apt_requirements.sh
@@ -3,5 +3,5 @@ set -e
 add-apt-repository ppa:cython-dev/master-ppa -y
 echo deb http://www.openfoam.org/download/ubuntu precise main > /etc/apt/sources.list.d/openfoam.list
 apt-get update -qq
-apt-get install -y build-essential git subversion libhdf5-serial-dev mpi-default-bin mpi-default-dev python-pip python-virtualenv python-dev cython python-vtk python-qt4 python-qt4-dev python-sip python-qt4-gl libqt4-scripttools libatlas-dev libatlas3gf-base
+apt-get install -y build-essential git subversion libhdf5-serial-dev mpi-default-bin mpi-default-dev python-pip python-virtualenv python-dev cython python-vtk python-qt4 python-qt4-dev python-sip python-qt4-gl libqt4-scripttools libatlas-dev libatlas3gf-base python-imaging
 apt-get install -y --force-yes openfoam222
