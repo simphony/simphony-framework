40896dcbc132a0fe6c468d11d1c230f16ba73cce
Stefano Borini <sborini@enthought.com>
2016-12-05 15:49:50 +0000

    Verifying kratos issues by logging

--- tests/test_engine_import.py
+++ tests/test_engine_import.py
@@ -1,7 +1,19 @@
 import unittest
 import os
+import logging
 
 from stevedore.extension import ExtensionManager
+import logging
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
 
 ENGINES = [
     'lammps',
